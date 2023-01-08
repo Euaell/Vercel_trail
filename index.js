1d02df886f1ecaf6e93e95bb3ea715df3bb9a89e
Euael Eshete <euaelmeko@gmail.com>
2023-01-08 10:54:30 +0300

    Setting up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World! From Euael');
+    res.send('Hello World! From Euael Mekonen');
 });
 
 app.listen(3000, () => {
